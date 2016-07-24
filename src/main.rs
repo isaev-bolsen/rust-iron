d4ee3df26a80103d75fb0570ce55cdcfba010995
isaev-bolsen <al_mamistov@mail.ru>
2016-07-24 20:59:28 +0300

    I was able to extract method!

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ fn main() {
    Iron::new(|_:&mut Request|{
 
 	let content_type="text/html".parse::<Mime>().unwrap();
-        Ok(Response::with((content_type,status::Ok, "Hail Torvalds!\n")))
+        Ok(Response::with((content_type, status::Ok, get_answer_text())))
 
    }).http("localhost:3000").unwrap();
+}
+
+fn get_answer_text() -> String
+{
+    String::from("Hail Torvalds!\n")
 }
